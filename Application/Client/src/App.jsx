271f63a66f9aca5ad20db4e484230c68610dbdea
unknown <kit28.24bcs010@gmail.com>
2025-12-23 11:26:39 +0530

    app js update

--- Application/Client/src/App.jsx
+++ Application/Client/src/App.jsx
@@ -33,40 +33,246 @@
 // }
 
 // export default App
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-function App(){
-  const [users, setUsers] = useState([]);//state manage user[1,2]
-  const[name,SetName]=useState("")
-  const[email,setEmail] = useState("")
+// function App(){
+//   const [users, setUsers] = useState([]);//state manage user[1,2]
+//   const[name,SetName]=useState("")
+//   const[email,setEmail] = useState("")
 
-  useEffect(()=>{
-    fetch("http://localhost:5000/users")
-    .then((res)=> res.json())//res in json format
-    .then((data)=> setUsers(data));
-  },[]);
-  const addUser=()=>{
-    fetch("http://localhost:5000/users")
+//   useEffect(()=>{
+//     fetch("http://localhost:5000/users")
+//     .then((res)=> res.json())//res in json format
+//     .then((data)=> setUsers(data));
+//   },[]);
+//   const addUser=()=>{
+//     fetch("http://localhost:5000/users")
     
-  }
+//   }
 
-  return(
+//   return(
+//     <div>
+//       <h1>User List</h1>
+//       <input placeholder="Name" value={name} onChange={(e) => SetName(e.target.value)}
+//       />
+//       <input placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)}
+//       />
+//       <button onClick={addUser}>Add</button>
+//       <ul>
+//         {users.map((u)=>(
+//           <li key={u.id}>
+//             {u.name} - {u.email} 
+//           </li> // iterate and print
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import { useEffect, useState } from "react";
+// function App() {
+//   const [users, setUsers] = useState([]);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   useEffect(() => {
+//     fetch("http://localhost:5000/users")
+//       .then((res) => res.json())
+//       .then((data) => setUsers(data));
+//   }, []);
+//   const addUser = () => {
+//     fetch("http://localhost:5000/users", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ name, email }),
+//     })
+//       .then((res) => res.json())
+//       .then((newUser) => {
+//         setUsers([...users, newUser]);
+//         setName("");
+//         setEmail("");
+//       });
+//   };
+//   const deleteUser = (id) => {
+//     fetch(`http://localhost:5000/users/${id}`, {
+//       method: "DELETE",
+//     }).then(() => {
+//       setUsers(users.filter((u) => u.id !== id));
+//     });
+//   };
+//   return (
+//     <div>
+//       <h1>User List</h1>
+//       <input
+//         placeholder="Name"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <input
+//         placeholder="Email"
+//         value={email}
+//         onChange={(e) => setEmail(e.target.value)}
+//       />
+//       <button onClick={addUser}>Add</button>
+//       <ul>
+//         {users.map((u) => (
+//           <li key={u.id}>
+//             {u.name} - {u.email}
+//             <button onClick={() => deleteUser(u.id)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export default App;
+// import React, { useEffect, useState } from "react";
+
+// function App(){
+//   const [users, setUsers] = useState([]);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const[editingId,setEditingId]=useState(null);
+
+//   useEffect(()=>{
+//     fetch("http://localhost:5000/users")
+//     .then((res)=> res.json())
+//     .then((data)=> setUsers(data));
+//   },[]);
+
+//   const addUser = () =>{
+//     if ( !name || !email) return;
+//     fetch("http://localhost:5000/users",{
+//       method: "POST",
+//       headers: { "Content-Type":"application/json"},
+//       body: JSON.stringify({name, email}),
+//     })
+//     .then((res)=> res.json())
+//     .then((newUser)=>{
+//       setUsers([...users, newUser]);
+//       setName("");
+//       setEmail("");
+//     });
+//   };
+
+//   return(
+//     <div>
+//       <h1>User List</h1>
+//       <input
+//           placeholder="Name"
+//           value={name}
+//           onChange={(e)=> setName(e.target.value)}
+//           />
+//           <input
+//              placeholder="Email"
+//              value={email}
+//              onChange={(e)=> setEmail(e.target.value)}
+//              />
+//             <button onClick={addUser}>Add</button>
+//       <ul>
+//         {users.map((u)=>(
+//           <li key={u._id}>
+//             {u.name} - {u.email}
+//             <button type="button" onClick={()=>addUser(u)}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export default App;
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { useEffect, useState } from "react";
+function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [editingId, setEditingId] = useState(null); 
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  const addUser = () => {
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email }),
+    })
+      .then((res) => res.json())
+      .then((newUser) => {
+        setUsers([...users, newUser]);
+        setName("");
+        setEmail("");
+      });
+  };
+  const deleteUser = (id) => {
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setUsers(users.filter((u) => u.id !== id));
+    });
+  };
+  const editUser()
+  return (
     <div>
       <h1>User List</h1>
-      <input placeholder="Name" value={name} onChange={(e) => SetName(e.target.value)}
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      <input placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)}
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <button onClick={addUser}>Add</button>
       <ul>
-        {users.map((u)=>(
-          <li key={u.id}>
-            {u.name} - {u.email} 
-          </li> // iterate and print
+        {users.map((u) => (
+          <li key={u._id}>
+            {u.name} - {u.email}
+            <button onClick={() => deleteUser(u.id)}>Delete</button>
+           <button type ="button" onClick={()=>editUser(u)}>{editingId==u._id? "Save":"Edit"}</button>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
-
 export default App;
--- Application/Server/app.js
+++ Application/Server/app.js
@@ -10,40 +10,208 @@
 //     console.log("Server is running on http://localhost:3000")
 // });
 
+// const express = require("express");
+// const cors = require("cors");
+
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// var users = [
+//     { id :1, name : "Alex", email: "alex@text.com"},
+//     { id :2, name : "John", email: "john@text.com"}, //static dummmy database in bend
+// ];
+
+// app.get("/users", (req,res)=>{//respond
+//     res.json(users);
+// });
+
+// app.post("/users", (req,res)=>{//both req and res
+//     const newUser = { id: Date.now(),...req.body }; //(...=spred operator)
+//     users.push(newUser);//3
+//     res.json(newUser);
+// });
+// //localhst:5000/users/2(id)
+// app.put("/users/:id", (req,res)=>{
+//     const id = Number(req.params.id);
+//     users = users.map((u)=>(u,id === id ? { ...u, ...req.body } : u));
+//     res.json({ message: "Updated" });
+// });
+
+// app.delete("/users/:id", (req,res)=>{ //only res
+//     const id = Number(req.params.id);
+//     users = users.filter((u) => u.id !== id);
+//     res.json({ message: "Deleted"})
+// })
+// app.listen(5000,()=>{
+//     console.log("Server running on port 5000");
+// });
+// ct express = require("express");
+// const cors = require("cors");
+// const mongoose=require("mongoose");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// // var users = [
+// //     { id :1, name : "Alex", email: "alex@text.com"},
+// //     { id :2, name : "John", email: "john@text.com"}, //static dummmy database in bend
+// // ];
+// mongoose
+//     .connect("mongodb://localhost:27017/mern-mongo")
+//     .then(()=>{
+//         console.log("MongoDB connected");
+//     })
+//     .catch((err)=>{
+//             console.error("Error connecting to mongodb:");
+//     });
+//     const userSchema = new mongoose.Schema({
+//         name:{
+//             required: true,
+//             type:String,
+//         },
+//         email:{
+//             required:true,
+//             type:String,
+//         },
+//     }); 
+// app.get("/users", (req,res)=>{//respond
+//     res.json(users);
+// });
+
+// app.post("/users", (req,res)=>{//both req and res
+//     const newUser = { id: Date.now(),...req.body }; //(...=spred operator)
+//     users.push(newUser);//3
+//     res.json(newUser);
+// });
+// //localhst:5000/users/2(id)
+// app.put("/users/:id", (req,res)=>{
+//     const id = Number(req.params.id);
+//     users = users.map((u)=>(u,id === id ? { ...u, ...req.body } : u));
+//     res.json({ message: "Updated" });
+// });
+
+// app.delete("/users/:id", (req,res)=>{ //only res
+//     const id = Number(req.params.id);
+//     users = users.filter((u) => u.id !== id);
+//     res.json({ message: "Deleted"})
+// })
+// app.listen(5000,()=>{
+//     console.log("Server running on port 5000");
+// });
+
+/*const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/login",(req,res)=>{
+    res.send("Login Page");
+});
+//start the server:
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000");
+});*/
+
+/*const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/login",(req,res)=>{
+    res.send("Login Page");
+});
+//start the server:
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000");
+});*/
+
+/*const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/login",(req,res)=>{
+    res.send("Login Page");
+});
+//start the server:
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000");
+});*/
+
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors");//cross-orgin managing handels's request
+const mongoose = require("mongoose");//it's a library:
 
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json());//for convert json formate
+//let id = 0;
+/*var users = [
+    { id :1, name : "Mohith", email: "mohith@tom.com"},
+    { id :2, name : "Srimathi", email: "srimathi@jerry.com"},
+];*/
+mongoose
+    .connect("mongodb://localhost:27017/learning-mern")
+    .then(()=>{
+        console.log("MongoDB connected");
+    })
+    .catch((err)=>{
+        console.error("Error connecting to MongoDB:");
+    });
 
-var users = [
-    { id :1, name : "Alex", email: "alex@text.com"},
-    { id :2, name : "John", email: "john@text.com"}, //static dummmy database in bend
-];
+const userSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+    },
+    email: {
+        required: true,
+        type: String,
+    },
+});
 
-app.get("/users", (req,res)=>{//respond
+const UserModel = mongoose.model("User",userSchema);
+app.get("/users", async (req,res)=>{
+    try {
+        const users = await UserModel.find({});
     res.json(users);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
 });
 
-app.post("/users", (req,res)=>{//both req and res
-    const newUser = { id: Date.now(),...req.body }; //(...=spred operator)
-    users.push(newUser);//3
-    res.json(newUser);
+app.post("/users", async (req,res)=>{
+    try{
+        const newUser = new UserModel(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    }catch (error){
+        res.status(500).json({ message: error.message });
+    }
 });
-//localhst:5000/users/2(id)
-app.put("/users/:id", (req,res)=>{
+
+/*app.put("/users/:id", (req,res)=>{
     const id = Number(req.params.id);
-    users = users.map((u)=>(u,id === id ? { ...u, ...req.body } : u));
+    users = users.map((u)=>(u.id === id ? { ...u, ...req.body } : u));
     res.json({ message: "Updated" });
 });
 
-app.delete("/users/:id", (req,res)=>{ //only res
+app.delete("/users/:id", (req,res)=>{
     const id = Number(req.params.id);
     users = users.filter((u) => u.id !== id);
     res.json({ message: "Deleted"})
-})
-app.listen(5000,()=>{
+});*/
+
+app.listen(5000, ()=>{
     console.log("Server running on port 5000");
 });
